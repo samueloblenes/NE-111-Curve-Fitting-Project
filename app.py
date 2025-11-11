5877873dda0dbf9af18ee077ec41dde5fa5badc8
samueloblenes <oblenessamuel@gmail.com>
2025-11-11 10:24:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 
+
+# Page formating
+st.set_page_config(
+    page_title="NE 111 Project",
+    page_icon="📊",
+    layout="wide",  # optional: "centered" or "wide"
+    initial_sidebar_state="expanded"  # optional
+)
+
+page_title = "📊 Curve Fitting Web App"
