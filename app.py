faf7d3e3941364defe135e11a3261a275d747ca6
samueloblenes <oblenessamuel@gmail.com>
2025-11-17 22:11:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def data_entry(entry_method, unique_prefix):
 
 def plot(data_confirmed, dataframe, dist_name, num_points  = 300, xi = None, xf = None): 
     if data_confirmed and not dataframe.empty: # If data is confirmed and the dataframe is not empty, display the graph and table
-        col1, col2 = st.columns([1,3])
+        col1, col2 = st.columns(2)
         col1.subheader("Data")
         col2.subheader("Distribution")
     
@@ -83,39 +83,41 @@ def plot(data_confirmed, dataframe, dist_name, num_points  = 300, xi = None, xf 
         
         orig_df, fit_df = fit(df_to_plot, dist_name, xi = xi, xf = xf, num_points = int(num_points)) 
         
+       
+        
+        # if no numerical data was entered, display and error
+        if df_to_plot.empty:
+            st.error("No numeric data available to plot.") # Error message if no data is enetred and the program proceeds to try and graph
+            
+        else:
+            
+            fig, ax = plt.subplots()
+            ax.hist(orig_df["Y-Axis"], bins=30, density=True, alpha=0.5, label="Data Histogram") # create histogram of the entered data
+            ax.plot(fit_df["X-Axis"], fit_df["Y-Axis"], color='red', lw=2, label="Fitted Curve") # Overlay the fitted curve
+            # Display in Streamlit
+            st.pyplot(fig)
+
         with col1:
             st.write("Entered Data")
             st.dataframe(orig_df)
+        with col2:
             st.write("Fit data")
             st.dataframe(fit_df)
-        
-        with col2:
-            # if no numerical data was entered, display and error
-            if df_to_plot.empty:
-                st.error("No numeric data available to plot.") # Error message if no data is enetred and the program proceeds to try and graph
-                
-            else:
-               
-                fig, ax = plt.subplots()
-                ax.hist(orig_df["Y-Axis"], bins=30, density=True, alpha=0.5, label="Data Histogram") # create histogram of the entered data
-                ax.plot(fit_df["X-Axis"], fit_df["Y-Axis"], color='red', lw=2, label="Fitted Curve") # Overlay the fitted curve
-                # Display in Streamlit
-                st.pyplot(fig)
-        
     else: 
         st.write("Once you enter and confirm your data a graph will apear here") # if data is not confirmed, display this message
 
 # defining functions that collects graph appearance settings
-def get_histogram_settings():
-    st.header("Histogram Appearance & Styling")
-    color = st.color_picker("Bar Color", "#1f77b4")
-    edgecolor = st.color_picker("Edge Color", "#000000")
-    linewidth = st.slider("Edge Line Width", 0.0, 5.0, 1.0)
-    alpha = st.slider("Transparency (alpha)", 0.0, 1.0, 0.8)
-    rwidth = st.slider("Relative Bar Width", 0.1, 1.0, 0.8)
-    histtype = st.selectbox("Histogram Type", options=["bar", "barstacked", "step", "stepfilled"])
-    orientation = st.selectbox("Orientation", options=["vertical", "horizontal"])
-    label = st.text_input("Histogram Label", "Histogram")
+def get_histogram_settings(key):
+    
+    st.subheader("Histogram Appearance & Styling")
+    color = st.color_picker("Bar Color", "#1f77b4", key = key)
+    edgecolor = st.color_picker("Edge Color", "#000000", key = key+"_edge")
+    linewidth = st.slider("Edge Line Width", 0.0, 5.0, 1.0, key = key+"_line")
+    alpha = st.slider("Transparency (alpha)", 0.0, 1.0, 0.8, key = key+"_alpha")
+    rwidth = st.slider("Relative Bar Width", 0.1, 1.0, 0.8, key = key+"_rwidth")
+    histtype = st.selectbox("Histogram Type", options=["bar", "barstacked", "step", "stepfilled"], key = key+"_histtype")
+    orientation = st.selectbox("Orientation", options=["vertical", "horizontal"], key = key+"_orientation")
+    label = st.text_input("Histogram Label", "Histogram", key = key+"_label")
 
     appearance_settings = {
         "color": color,
@@ -128,20 +130,19 @@ def get_histogram_settings():
         "label": label
     }
 
-    st.header("Axis & Layout")
-    xlabel = st.text_input("X-axis Label", "Value")
-    ylabel = st.text_input("Y-axis Label", "Frequency")
-    title = st.text_input("Plot Title", "Histogram")
-    
+    st.subheader("Axis & Layout")
+    xlabel = st.text_input("X-axis Label", "Value", key = key+"_xlabel")
+    ylabel = st.text_input("Y-axis Label", "Frequency", key = key+"_ylabel")
+    title = st.text_input("Plot Title", "Histogram", key = key+"_title")
+
     col_dist_1, col_dist2 = st.columns(2)
     
     with col_dist_1:
-        hist_xlim_min = st.number_input("Histogram X-axis min", value=0.0)
-        hist_ylim_min = st.number_input("Histogram Y-axis min", value=0.0)
+        hist_xlim_min = st.number_input("Histogram X-axis min", value=0.0, key=key+"_xmin")
+        hist_ylim_min = st.number_input("Histogram Y-axis min", value=0.0, key=key+"_ymin")
     with col_dist2:
-        hist_xlim_max = st.number_input("Histogram X-axis max", value=1.0)
-        hist_ylim_max = st.number_input("Histogram Y-axis max", value=1.0)
-    
+        hist_xlim_max = st.number_input("Histogram X-axis max", value=1.0, key=key+"_xmax")
+        hist_ylim_max = st.number_input("Histogram Y-axis max", value=1.0, key=key+"_ymax")
 
     axis_settings = {
         "xlabel": xlabel,
@@ -153,12 +154,12 @@ def get_histogram_settings():
 
     return appearance_settings, axis_settings
 
-def get_distribution_plot_settings():
-    st.header("Distribution Plot Appearance & Styling")
-    dist_color = st.color_picker("Distribution Line Color", "#ff7f0e")
-    dist_linewidth = st.slider("Distribution Line Width", 0.0, 5.0, 2.0)
-    dist_alpha = st.slider("Distribution Transparency (alpha)", 0.0, 1.0, 1.0)
-    dist_label = st.text_input("Distribution Label", "Distribution")
+def get_distribution_plot_settings(key):
+    st.subheader("Distribution Plot Appearance & Styling")
+    dist_color = st.color_picker("Distribution Line Color", "#ff7f0e", key=key)
+    dist_linewidth = st.slider("Distribution Line Width", 0.0, 5.0, 2.0, key=key+"_linewidth")
+    dist_alpha = st.slider("Distribution Transparency (alpha)", 0.0, 1.0, 1.0, key=key+"_alpha")
+    dist_label = st.text_input("Distribution Label", "Distribution", key=key+"_label")
 
     dist_appearance_settings = {
         "color": dist_color,
@@ -167,20 +168,20 @@ def get_distribution_plot_settings():
         "label": dist_label,
     }
 
-    st.header("Distribution Plot Axis & Layout")
-    dist_xlabel = st.text_input("Distribution X-axis Label", "Value")
-    dist_ylabel = st.text_input("Distribution Y-axis Label", "Density")
-    dist_title = st.text_input("Distribution Plot Title", "Distribution Plot")
-    
-    
+    st.subheader("Distribution Plot Axis & Layout")
+    dist_xlabel = st.text_input("Distribution X-axis Label", "Value", key=key+"_xlabel")
+    dist_ylabel = st.text_input("Distribution Y-axis Label", "Density", key=key+"_ylabel")
+    dist_title = st.text_input("Distribution Plot Title", "Distribution Plot", key=key+"_title")
+
+
     col_dist_1, col_dist2 = st.columns(2)
     
     with col_dist_1:
-        dist_xlim_min = st.number_input("Distribution X-axis min", value=0.0)
-        dist_ylim_min = st.number_input("Distribution Y-axis min", value=0.0)
+        dist_xlim_min = st.number_input("Distribution X-axis min", value=0.0, key=key+"_xmin")
+        dist_ylim_min = st.number_input("Distribution Y-axis min", value=0.0, key=key+"_ymin")
     with col_dist2:
-        dist_xlim_max = st.number_input("Distribution X-axis max", value=1.0)
-        dist_ylim_max = st.number_input("Distribution Y-axis max", value=1.0)
+        dist_xlim_max = st.number_input("Distribution X-axis max", value=1.0, key=key+"_xmax")
+        dist_ylim_max = st.number_input("Distribution Y-axis max", value=1.0, key=key+"_ymax")
 
     dist_axis_settings = {
         "xlabel": dist_xlabel,
@@ -284,10 +285,21 @@ tab1, tab2= st.tabs(["Auto Fit", "Manual Fit"])
 
 ########## Tab1 Auto curve fitting ##########
 with tab1:    
-    st.text("Auto curv fitting. create default paramters and make so that if this tab is selected fitting is done with the default paramater, include some plot appearance customization")
+    tab1_col1, tab1_col2 = st.columns([1,3])
+    with tab1_col1:
 
-    st.divider()
-    plot(st.session_state.Dataconfirmed,st.session_state.df, st.session_state.dist_name, st.session_state.num_points) # call plot function to display graph 
+        hist_expander = st.expander("Histogram Appearance & Styling")
+        dist_expander = st.expander("Distribution Plot Appearance & Styling")
+
+        with hist_expander:
+            hist_appearance_settings, hist_axis_settings = get_histogram_settings(key="auto_hist")
+
+        with dist_expander:
+            dist_appearance_settings, dist_axis_settings = get_distribution_plot_settings(key="auto_dist")
+   
+    with tab1_col2:
+        plot(st.session_state.Dataconfirmed,st.session_state.df, st.session_state.dist_name, st.session_state.num_points) # call plot function to display graph
+     
 ########## Tab2, Manual curve fitting ##########
 with tab2:
     st.text("Configure curve fitting settings")
@@ -304,12 +316,20 @@ with tab2:
     fixed_scale = None
     
     st.divider()
+        
+    tab1_col1, tab1_col2 = st.columns([1,3])
+    with tab1_col1:
+        hist_expander = st.expander("Histogram Appearance & Styling")
+        dist_expander = st.expander("Distribution Plot Appearance & Styling")
+        
+        with hist_expander:
+            hist_appearance_settings, hist_axis_settings = get_histogram_settings(key="man_hist")
 
-    st.text("Configure graph appearance")
-    
-    st.divider()
-    plot(st.session_state.Dataconfirmed,st.session_state.df, st.session_state.dist_name, st.session_state.num_points, xi = xi, xf = xf) # call plot function to display graph 
+        with dist_expander:
+            dist_appearance_settings, dist_axis_settings = get_distribution_plot_settings(key="man_dist")
 
+    with tab1_col2:
+        plot(st.session_state.Dataconfirmed,st.session_state.df, st.session_state.dist_name, st.session_state.num_points, xi = xi, xf = xf) # call plot function to display graph 
 
 # Samuel O'Blenes NE 111 Final project
 
