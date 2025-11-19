f0924fa52f3e3f68034b40305312e49f46a5fa59
samueloblenes <oblenessamuel@gmail.com>
2025-11-19 13:38:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,12 +69,9 @@ def data_entry(entry_method, unique_prefix):
 
 # defining function that plots the entered data and the fit data
 
-def plot(data_confirmed, dataframe, dist_name, num_points  = 300, xi = None, xf = None): 
+def plot(data_confirmed, dataframe, dist_name, plt_appearance_settings, hist_appearance_settings, axis_settings, num_points  = 300, xi = None, xf = None): 
     if data_confirmed and not dataframe.empty: # If data is confirmed and the dataframe is not empty, display the graph and table
-        col1, col2 = st.columns(2)
-        col1.subheader("Data")
-        col2.subheader("Distribution")
-    
+        
         # prepare/clean entered date
         df_to_plot = dataframe # define the dataframe to plot
         for col in df_to_plot.columns:
@@ -92,10 +89,33 @@ def plot(data_confirmed, dataframe, dist_name, num_points  = 300, xi = None, xf 
         else:
             
             fig, ax = plt.subplots()
-            ax.hist(orig_df["Y-Axis"], bins=30, density=True, alpha=0.5, label="Data Histogram") # create histogram of the entered data
-            ax.plot(fit_df["X-Axis"], fit_df["Y-Axis"], color='red', lw=2, label="Fitted Curve") # Overlay the fitted curve
+            ax.hist(orig_df["Y-Axis"], bins=30, density=True, **hist_appearance_settings) # create histogram of the entered data, unpack settings dictionary
+            ax.plot(fit_df["X-Axis"], fit_df["Y-Axis"], **plt_appearance_settings) # Overlay the fitted curve, unpack settings dictionary
+
+            # Set axis labels and title from the axis_setting dictionary
+            ax.set_xlabel(axis_settings["xlabel"])
+            ax.set_ylabel(axis_settings["ylabel"])
+            ax.set_title(axis_settings["title"])
+            
+            
+            # set axis limits with some padding based on entered data
+            
+            nonzero = fit_df["Y-Axis"].astype(float) != 0
+            if nonzero.any():
+                last_nonzero_idx = nonzero[nonzero].index[-1]
+                df_trimmed = fit_df.loc[:last_nonzero_idx]
+            else:
+                df_trimmed = fit_df.iloc[0:0]
+
+            ax.set_xlim(0, max(df_trimmed["X-Axis"]) * 1.1)
+            ax.set_ylim(0, max(df_trimmed["Y-Axis"]) * 1.1)
+
             # Display in Streamlit
             st.pyplot(fig)
+            
+        col1, col2 = st.columns(2)
+        col1.subheader("Data")
+        col2.subheader("Distribution")
 
         with col1:
             st.write("Entered Data")
@@ -135,21 +155,11 @@ def get_histogram_settings(key):
     ylabel = st.text_input("Y-axis Label", "Frequency", key = key+"_ylabel")
     title = st.text_input("Plot Title", "Histogram", key = key+"_title")
 
-    col_dist_1, col_dist2 = st.columns(2)
-    
-    with col_dist_1:
-        hist_xlim_min = st.number_input("Histogram X-axis min", value=0.0, key=key+"_xmin")
-        hist_ylim_min = st.number_input("Histogram Y-axis min", value=0.0, key=key+"_ymin")
-    with col_dist2:
-        hist_xlim_max = st.number_input("Histogram X-axis max", value=1.0, key=key+"_xmax")
-        hist_ylim_max = st.number_input("Histogram Y-axis max", value=1.0, key=key+"_ymax")
-
     axis_settings = {
         "xlabel": xlabel,
         "ylabel": ylabel,
         "title": title,
-        "xlim": (hist_xlim_min, hist_xlim_max),
-        "ylim": (hist_ylim_min, hist_ylim_max),
+
     }
 
     return appearance_settings, axis_settings
@@ -168,30 +178,7 @@ def get_distribution_plot_settings(key):
         "label": dist_label,
     }
 
-    st.subheader("Distribution Plot Axis & Layout")
-    dist_xlabel = st.text_input("Distribution X-axis Label", "Value", key=key+"_xlabel")
-    dist_ylabel = st.text_input("Distribution Y-axis Label", "Density", key=key+"_ylabel")
-    dist_title = st.text_input("Distribution Plot Title", "Distribution Plot", key=key+"_title")
-
-
-    col_dist_1, col_dist2 = st.columns(2)
-    
-    with col_dist_1:
-        dist_xlim_min = st.number_input("Distribution X-axis min", value=0.0, key=key+"_xmin")
-        dist_ylim_min = st.number_input("Distribution Y-axis min", value=0.0, key=key+"_ymin")
-    with col_dist2:
-        dist_xlim_max = st.number_input("Distribution X-axis max", value=1.0, key=key+"_xmax")
-        dist_ylim_max = st.number_input("Distribution Y-axis max", value=1.0, key=key+"_ymax")
-
-    dist_axis_settings = {
-        "xlabel": dist_xlabel,
-        "ylabel": dist_ylabel,
-        "title": dist_title,
-        "xlim": (dist_xlim_min, dist_xlim_max),
-        "ylim": (dist_ylim_min, dist_ylim_max),
-    }
-
-    return dist_appearance_settings, dist_axis_settings
+    return dist_appearance_settings
     
 
 # Samuel O'Blenes
@@ -252,26 +239,16 @@ st.session_state.dist_name = st.selectbox(
     )
 
 latex_formulas = [
-    # norm
-    r"f(x) = \frac{1}{\sqrt{2\pi}} \exp\left(-\frac{x^2}{2}\right)",
-    # expon
-    r"f(x) = \exp(-x), \quad x \geq 0",
-    # gamma
-    r"f(x, a) = \frac{1}{\Gamma(a)} x^{a-1} \exp(-x), \quad x \geq 0",
-    # beta
-    r"f(x, a, b) = \frac{\Gamma(a+b)}{\Gamma(a)\Gamma(b)} x^{a-1}(1-x)^{b-1},\quad 0 < x < 1",
-    # uniform
-    r"f(x) = 1, \quad 0 \leq x \leq 1",
-    # lognorm
-    r"f(x,s) = \frac{1}{s x \sqrt{2\pi}} \exp\left(-\frac{(\ln x)^2}{2s^2}\right),\quad x > 0",
-    # weibull_min
-    r"f(x,c) = c x^{c-1} \exp(-x^{c}),\quad x \geq 0",
-    # chi2
-    r"f(x,k) = \frac{1}{2^{k/2} \Gamma(k/2)} x^{k/2-1} e^{-x/2},\quad x \geq 0",
-    # laplace
-    r"f(x) = \frac{1}{2} \exp(-|x|)",
-    # cauchy
-    r"f(x) = \frac{1}{\pi (1 + x^2)}",
+    r"f(x) = \frac{1}{\sqrt{2\pi}} \exp\left(-\frac{x^2}{2}\right)", # norm
+    r"f(x) = \exp(-x), \quad x \geq 0", # expon
+    r"f(x, a) = \frac{1}{\Gamma(a)} x^{a-1} \exp(-x), \quad x \geq 0", # gamma
+    r"f(x, a, b) = \frac{\Gamma(a+b)}{\Gamma(a)\Gamma(b)} x^{a-1}(1-x)^{b-1},\quad 0 < x < 1", # beta
+    r"f(x) = 1, \quad 0 \leq x \leq 1", # uniform
+    r"f(x,s) = \frac{1}{s x \sqrt{2\pi}} \exp\left(-\frac{(\ln x)^2}{2s^2}\right),\quad x > 0", # lognorm
+    r"f(x,c) = c x^{c-1} \exp(-x^{c}),\quad x \geq 0", # weibull_min
+    r"f(x,k) = \frac{1}{2^{k/2} \Gamma(k/2)} x^{k/2-1} e^{-x/2},\quad x \geq 0", # chi2
+    r"f(x) = \frac{1}{2} \exp(-|x|)", # laplace
+    r"f(x) = \frac{1}{\pi (1 + x^2)}", # cauchy
 ]
 st.latex(latex_formulas[ ["norm", "expon", "gamma", "beta", "uniform", 
         "lognorm", "weibull_min", "chi2", "laplace", "cauchy"].index(st.session_state.dist_name)])
@@ -292,13 +269,13 @@ with tab1:
         dist_expander = st.expander("Distribution Plot Appearance & Styling")
 
         with hist_expander:
-            hist_appearance_settings, hist_axis_settings = get_histogram_settings(key="auto_hist")
+            hist_appearance_settings, axis_settings = get_histogram_settings(key="auto_hist")
 
         with dist_expander:
-            dist_appearance_settings, dist_axis_settings = get_distribution_plot_settings(key="auto_dist")
+            dist_appearance_settings = get_distribution_plot_settings(key="auto_dist")
    
     with tab1_col2:
-        plot(st.session_state.Dataconfirmed,st.session_state.df, st.session_state.dist_name, st.session_state.num_points) # call plot function to display graph
+        plot(st.session_state.Dataconfirmed, st.session_state.df, st.session_state.dist_name, dist_appearance_settings, hist_appearance_settings, axis_settings, st.session_state.num_points) # call plot function to display graph
      
 ########## Tab2, Manual curve fitting ##########
 with tab2:
@@ -322,14 +299,15 @@ with tab2:
         hist_expander = st.expander("Histogram Appearance & Styling")
         dist_expander = st.expander("Distribution Plot Appearance & Styling")
         
+
         with hist_expander:
             hist_appearance_settings, hist_axis_settings = get_histogram_settings(key="man_hist")
 
         with dist_expander:
-            dist_appearance_settings, dist_axis_settings = get_distribution_plot_settings(key="man_dist")
+            dist_appearance_settings = get_distribution_plot_settings(key="man_dist")
 
     with tab1_col2:
-        plot(st.session_state.Dataconfirmed,st.session_state.df, st.session_state.dist_name, st.session_state.num_points, xi = xi, xf = xf) # call plot function to display graph 
+        plot(st.session_state.Dataconfirmed, st.session_state.df, st.session_state.dist_name, dist_appearance_settings, hist_appearance_settings, axis_settings, st.session_state.num_points,xi, xf) # call plot function to display graph 
 
 # Samuel O'Blenes NE 111 Final project
 
